74edf217f6eb9e3e001ad41c378098296033a47f
Nurul Amin <namin2750@gmail.com>
2025-04-16 01:02:45 +0600

    add framworks

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,30 @@
+<div class="container">
+  <div>Header</div>
+     <div class="main">
+       <slot/>
+     </div>
+   <footer class="footer">
+     <div class="content has-text-centered">
+       <p class="has-text-white">
+         © 2024 Expense Tracker Developed by Nurul Amin. All rights reserved.
+       </p>
+     </div>
+   </footer>
+ </div>
+ 
+ <style>
+ 
+   .main{
+     border-left: 5px solid #006400;
+     border-right: 5px solid #006400;
+     /* height: 100vh; */
+   }
+   footer{
+     background-color: #006400 !important;
+     padding: 20px !important;
+   }
+ </style>
+ <script>
+   import 'bulma/css/bulma.min.css';
+   import '@fortawesome/fontawesome-free/css/all.css';
+ </script>
