6b4c7a899ec937e513c7005858325c81105f395f
Nurul Amin <namin2750@gmail.com>
2025-05-02 00:13:57 +0600

    fix

--- src/routes/english/+page.svelte
+++ src/routes/english/+page.svelte
@@ -148,7 +148,7 @@
 							</div>
 						{/if}
 						{#if $question_type !== 'Spelling'}
-							<div class="columns is-multiline is-mobile mt-3">
+							<!-- <div class="columns is-multiline is-mobile mt-3">
 								{#each $questions[current_question_index]?.options as option, i}
 									<div class="column is-6">
 										<ul>
@@ -167,6 +167,21 @@
 												{option}
 											</li>
 										</ul>
+									</div>
+								{/each}
+							</div> -->
+							<div class="options-grid mt-2">
+								{#each $questions[current_question_index]?.options as option, i}
+									<div
+										class="option-box {!$is_game_start || $is_game_pause ? 'disabled' : ''}"
+										onclick={() => handleOptionClick(option)}
+									>
+										{#if selected === option}
+											<i class="fa-solid fa-circle-check has-text-success"></i>
+										{:else}
+											<i class="fa-regular fa-circle has-text-dark"></i>
+										{/if}
+										<span class="has-text-weight-semibold has-text-dark">{option}</span>
 									</div>
 								{/each}
 							</div>
@@ -249,5 +264,41 @@
 		opacity: 0.5;
 		pointer-events: none !important;
 		cursor: not-allowed !important;
+	}
+	.options-grid {
+		display: grid;
+		grid-template-columns: repeat(2, 1fr);
+		gap: 1rem;
+	}
+
+	.option-box {
+		display: flex;
+		align-items: center;
+		justify-content: flex-start;
+		padding: 1rem;
+		border-radius: 0.5rem;
+		background-color: transparent;
+		cursor: pointer;
+		box-shadow: 0 2px 5px rgba(0, 0, 0, 0.1);
+		font-weight: 600;
+		font-size: 1.1rem;
+		transition:
+			background 0.3s,
+			transform 0.2s;
+	}
+
+	.option-box:hover {
+		background-color: #f8de9b;
+		transform: scale(1.005);
+	}
+
+	.option-box i {
+		margin-right: 0.5rem;
+	}
+
+	.option-box.disabled {
+		opacity: 0.5;
+		pointer-events: none;
+		cursor: not-allowed;
 	}
 </style>
