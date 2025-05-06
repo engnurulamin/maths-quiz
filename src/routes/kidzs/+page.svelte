ac830af15a35f4f8fa1897d5248fb4e82bce8668
Nurul Amin <namin2750@gmail.com>
2025-05-07 00:47:40 +0600

    conditonal button

--- src/routes/kidzs/+page.svelte
+++ src/routes/kidzs/+page.svelte
@@ -1,3 +1,7 @@
+<script>
+	let show = false;
+</script>
+
 <div class="columns">
 	<div class="column">
 		<div class="card has-background-warning has-text-centered m-4 p-4">
@@ -14,13 +18,26 @@
 						</div>
 					</div>
 					<div class="control">
-						<div class="field m-2">
-							<button class="button is-fullwidth is-info is-dark button-shadow">
-								<figure class="image is-24x24">
-									<img src="/icons/flag.png" alt="" />
-								</figure>
-								<span class="is-size-5 has-text-white ml-2">Bangla</span>
-							</button>
+						<div class="field m-2 is-grouped is-grouped-multiline">
+							{#if show}
+								<div class="control is-expanded">
+									<button class="button is-info has-text-white is-dark is-fullwidth">Vowel</button>
+								</div>
+								<div class="control is-expanded">
+									<button class="button is-link is-fullwidth">Consonant</button>
+								</div>
+							{/if}
+							{#if !show}
+								<button
+									class="button is-fullwidth is-info is-dark button-shadow"
+									onclick={() => (show = !show)}
+								>
+									<figure class="image is-24x24">
+										<img src="/icons/flag.png" alt="" />
+									</figure>
+									<span class="is-size-5 has-text-white ml-2">Bangla</span>
+								</button>
+							{/if}
 						</div>
 					</div>
 				</div>
@@ -29,20 +46,34 @@
 						<div class="field m-2">
 							<button class="button is-fullwidth is-danger is-dark button-shadow">
 								<figure class="image is-24x24">
-									<img src="/icons/school.png" alt="" />
+									<img src="/icons/language.png" alt="" />
 								</figure>
 								<span class="is-size-5 has-text-white ml-2">English</span>
 							</button>
 						</div>
 					</div>
 					<div class="control">
-						<div class="field m-2">
-							<button class="button is-fullwidth is-success is-dark button-shadow">
-								<figure class="image is-24x24">
-									<img src="/icons/nums.png" alt="" />
-								</figure>
-								<span class="is-size-5 has-text-white ml-2">Numbers</span>
-							</button>
+						<div class="field m-2 is-grouped is-grouped-multiline">
+							{#if show}
+								<div class="control is-expanded">
+									<button class="button is-success is-dark has-text-white is-fullwidth">1234</button
+									>
+								</div>
+								<div class="control is-expanded">
+									<button class="button is-link is-dark is-fullwidth has-text-white">১২৩৪</button>
+								</div>
+							{/if}
+							{#if !show}
+								<button
+									class="button is-fullwidth is-success is-dark button-shadow"
+									onclick={() => (show = !show)}
+								>
+									<figure class="image is-24x24">
+										<img src="/icons/nums.png" alt="" />
+									</figure>
+									<span class="is-size-5 has-text-white ml-2">Numbers</span>
+								</button>
+							{/if}
 						</div>
 					</div>
 				</div>
